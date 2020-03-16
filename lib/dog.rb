7608613e4b70ab66e96965a34638897426d2610b
Brandon Guffey <hypefox@gmail.com>
2020-03-16 23:16:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,21 @@
-class Dog 
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
+# class Dog 
+#   def name=(dog_name)
+#     @this_dogs_name = dog_name
+#   end
   
-  def name
-    @this_dogs_name
-  end
+#   def name
+#     @this_dogs_name
+#   end
+  
+#   def bark
+#     puts "woof!"
+#   end
+# end
+
+class Dog
+  attr_accessor :name
   
   def bark
     puts "woof!"
   end
 end
-
